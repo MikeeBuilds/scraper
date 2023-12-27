b4968bc4c57c897b1989ed2e84a6e77d7b1393ca
MikeeBuilds <citizensofandrometta@gmail.com>
2023-12-26 23:29:59 -0600

    fix build errors

--- .history/app/api/cron/route_20231226232801.ts
+++ .history/app/api/cron/route_20231226232801.ts
@@ -6,8 +6,6 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { NextResponse } from "next/server";
 
 export const maxDuration = 300;
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
 
 export async function GET() {
   try {
